6f97f653a5fd249f2ad67222befd4111ee15a47a
Daniel Hejduk <danielhejduk@disroot.org>
2025-04-28 20:00:03 +0200

    Adding basic freertos config

---
+++ include/FreeRTOSConfig.h
@@ -0,0 +1,12 @@
+#ifndef FREERTOS_CONFIG_H
+#define FREERTOS_CONFIG_H
+
+#define configMINIMAL_STACK_SIZE 128
+#define configMAX_PRIORITIES 5
+
+#define configUSE_16_BIT_TICKS 1
+#define configUSE_TICK_HOOK 1
+#define configUSE_IDLE_HOOK 1
+#define configUSE_PREEMPTION 1
+
+#endif // FREERTOS_CONFIG_H
